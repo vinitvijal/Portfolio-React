6e0a6744a43b018ea4a5fb2efab00ac5dc874b15
Vineet <vineet.22566@sscbs.du.ac.in>
2023-01-22 22:10:51 +0530

    resume file updating

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,20 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import pdf from '../asset/VINEET.pdf'
 import { Document, Page } from 'react-pdf'
 
 const Resume = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
     return (
         <div id='resume'>
             <section>
                 <div>
-                    <Document file='./VINEET.pdf'>
-                        <Page pageNumber={1} />
+                    <Document file={pdf}>
+                        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6}/>
                     </Document>
                 </div>
                 <div></div>
