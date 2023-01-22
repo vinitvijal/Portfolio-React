02483984abda0e423f75cf179c306fbbbf43dcbe
Vineet <vineet.22566@sscbs.du.ac.in>
2023-01-22 22:17:39 +0530

    resume file updating

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import pdf from '../asset/VINEET.pdf'
-import { Document, Page } from 'react-pdf'
+import { Document, Page, pdfjs} from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 const Resume = () => {
+
+    const resumeLink = 'https://github.com/vinitvijal/Portfolio-React/raw/6e0a6744a43b018ea4a5fb2efab00ac5dc874b15/src/asset/VINEET.pdf';
+
+    
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
@@ -13,7 +19,7 @@ const Resume = () => {
         <div id='resume'>
             <section>
                 <div>
-                    <Document file={pdf}>
+                    <Document file={resumeLink}>
                         <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6}/>
                     </Document>
                 </div>
